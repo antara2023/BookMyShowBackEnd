3da78682dac8f7e52203dd2dc75ffc316494c3a0
Antara Sarkar <antaranitdgp11@gmail.com>
2023-12-06 20:36:15 +0530

    backend completed

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import UserRoutes from "./routes/user.routes.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import connectToDB from "./config/dbconfig.js";
+import MovieRoutes from "./routes/movie.routes.js";
+import ShowRoutes from "./routes/show.routes.js";
+import cors from "cors";
+config();
+
+const app = express();
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", UserRoutes);
+app.use("/api/movie", MovieRoutes);
+app.use("/api/show", ShowRoutes);
+
+app.use("*", (req, res) => {
+  res.status(400).send("Page not found!");
+});
+
+app.listen(5050, async () => {
+  await connectToDB();
+  console.log(`Server is running on http://localhost:5050`);
+});
---
+++ routes/movie.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  createMovie,
+  getMovies,
+  getMoviebyId,
+} from "../controllers/movie.controller.js";
+import isLoggedIn from "../middlewares/authentication.js";
+import authorizedRoles from "../middlewares/authorization.js";
+const router = Router();
+
+router.post('/', isLoggedIn, authorizedRoles('SUPERADMIN', 'ADMIN'), createMovie);
+// router.post("/", createMovie);
+router.get("/list", getMovies);
+router.get("/detail/:movieId");
+// router.get('/count', getMovieCount)
+export default router;
